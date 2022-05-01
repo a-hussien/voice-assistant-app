60e7d3b74c37640771afc5e796bf16d4836720a4
ahmed hussien <a.hussien87@gmail.com>
2022-05-01 02:02:01 +0200

    Feature: Change Language

--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import './App.css';
 
 const App = () => {
+
+  const [lang, setLang] = useState('en-US')
+
+  useEffect(() => {
+    SpeechRecognition.startListening({continuous: true, language: lang});
+  }, [lang]);
+
+  const commands = [
+    {
+      command: ['clear', 'امسح'],
+      callback: ({resetTranscript}) => resetTranscript(),
+    },
+    {
+      command: 'open *',
+      callback: (site) => {
+        window.open(`https://${site}.com`)
+      }
+    },
+    {
+      command: 'search *',
+      callback: (search) => {
+        window.open(`https://www.google.com/search?q=${search}`)
+      }
+    },
+  ]
+
   const {
     transcript,
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
+  } = useSpeechRecognition({commands});
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
+  const changeLang = () => {
+    lang === 'en-US' ? setLang('ar-EG') : setLang('en-US')
+    resetTranscript()
+  }
+
   return (
     <div className='app'>
-      <div className='controls'>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      </div>
-      {
-          listening && (
-            <span className='listening'>
-              Listening: {listening ? 'On' : 'Off'}
-            </span>
-          ) 
-      }
+      <button onClick={ () => changeLang() }>{lang === 'en-US' ? 'AR':'EN'}</button>
+      <span className='listening'>
+        Listening: {listening ? 'on' : 'off'}
+      </span>
       
       <p className='App-header'>
        {transcript}
       </p>
+
     </div>
   );
 };
