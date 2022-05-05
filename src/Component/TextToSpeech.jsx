69886cdec7b3de433fc584cc3aef239bd6e5fa45
ahmed hussien <a.hussien87@gmail.com>
2022-05-05 02:30:25 +0200

    Created TextToSpeech component

--- src/Component/TextToSpeech.jsx
+++ src/Component/TextToSpeech.jsx
@@ -1,18 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import SpeechRecognition from 'react-speech-recognition';
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 const TextToSpeech = () => {
 
     const navigate = useNavigate();
+
+    const [value, setValue] = useState('');
+    const { speak, speaking, cancel, voices } = useSpeechSynthesis();
 
     useEffect(() => {
         SpeechRecognition.abortListening();
     }, []);
 
   return (
-    <div>
-        This is a TextToSpeech page
+    <div className='app'>
+
+        <textarea 
+        rows="15" 
+        className='textRec'
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        >
+
+        </textarea>
+
+        {
+            !speaking ? (
+              
+                <button onClick={() => speak({ text: value, voice: voices[4] })}>Speak</button>
+                
+            )
+            :
+            (
+                <button type="button" onClick={ cancel }> Stop </button>
+            )
+        }
+        
         <button className="btn"
         onClick={ () => {
             navigate('/')
